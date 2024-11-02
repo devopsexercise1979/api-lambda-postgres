35b012d4bb64ef42a8ce17c6d3f715e7e83d8ad0
devopsexercise1979 <109491325+devopsexercise1979@users.noreply.github.com>
2024-11-02 16:00:27 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_rds_instance" "postgres" {
+resource "aws_db_instance" "postgres" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
